ec6270b8581e15e26999356ed2587a70bbe278b3
Mohamed Charfi <mohamed.charfi@epfl.ch>
2023-01-05 21:53:54 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a deep lneural network to detect numbers from the mnist dataset',
+    author='mohamed charfi',
+    license='',
+)
